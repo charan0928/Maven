4811ffd00fd8aaec8351d5c74b028306fa159e48
Ubuntu <ubuntu@ip-172-31-22-247.eu-north-1.compute.internal>
2024-12-17 23:51:32 +0000

    Initial commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test; // Correct import for JUnit 4
+import static org.junit.Assert.assertTrue; // Correct static import for assertion
+
+public class AppTest {
+    @Test // Indicates this is a test case
+    public void shouldAnswerWithTrue() {
+        assertTrue(true); // Valid assertion method in JUnit 4
+    }
+}
